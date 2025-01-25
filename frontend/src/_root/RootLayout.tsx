f8277f0c08f9d19f45fd71445ac9917154d03e76
Nikhil Kumar <thenikhilkumar1@gmail.com>
2025-01-25 21:58:45 +0530

    pb-10 added to rootlayout

--- frontend/src/_root/RootLayout.tsx
+++ frontend/src/_root/RootLayout.tsx
@@ -3,26 +3,26 @@ import { Outlet } from "react-router-dom";
 import Topbar from "@/components/shared/Topbar";
 import Bottombar from "@/components/shared/Bottombar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const RootLayout = () => {
-  // const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
 
   // Listen for window resize changes
-  // useEffect(() => {
-  //   const handleResize = () => {
-  //     setIsMobile(window.innerWidth <= 768);
-  //   };
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
 
-  //   window.addEventListener("resize", handleResize);
-  //   return () => window.removeEventListener("resize", handleResize);
-  // }, []);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
     <div className="w-full md:flex">
       <Topbar />
       <LeftSidebar />
 
-      <section className={`flex flex-1 h-full`}>
+      <section className={`flex flex-1 h-full pt-8 ${isMobile ? "pb-10" : ""}`}>
         <Outlet />
       </section>
 
